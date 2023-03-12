9137712143482e13103be15a61b3131965c1822c
Dire <saber.direwolf@gmail.com>
2023-03-12 03:47:26 -0700

    Update pagination_remove_button_mod.sh

--- pagination_remove_button_mod.sh
+++ pagination_remove_button_mod.sh
@@ -2,10 +2,7 @@
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 FILE="node_modules/@acegoal07/discordjs-pagination/lib/MessagePagination.js"
-#TEXT="return pagination.edit\({ components: \[await DisabledButtonCreator\(paginationInfo.buttonList\)\] }\);"
 TEXT="return pagination.edit({ components: [await DisabledButtonCreator(paginationInfo.buttonList)] });"
 REPLACEMENT="return pagination.edit({ components: [] });"
-
-#grep -F "$TEXT" $FILE
 
 perl $DIR/str_replace.pl "$TEXT" "$REPLACEMENT" $FILE
